4e592e2c25460f34d0c31bed9a55409f7399b049
Bhukya Mourya Vardhan Rathod <mouryabhukya04@gmail.com>
2025-07-23 11:27:17 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot',
+    version='0.1',
+    author='Bhukya Mourya Vardhan Rathod',
+    author_email='mouryabhukya04@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
